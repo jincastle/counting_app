e39ad5ec13018fe3a0df7bc0d8520e2e7bf0886a
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2023-01-04 19:15:09 +0900

    숫자 세기

---
+++ app/src/main/java/com/project/counting_app/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.project.counting_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val numberTextView = findViewById<TextView>(R.id.numberTextView)
+        val resetButton = findViewById<Button>(R.id.resetButton)
+        val plusButton = findViewById<Button>(R.id.plusButtonView)
+
+        var number = 0
+
+
+        resetButton.setOnClickListener {
+            number=0
+            numberTextView.text = number.toString()
+            Log.i("onClick","리셋 된 숫자는 $number")
+        }
+
+        plusButton.setOnClickListener{
+            number += 1
+            numberTextView.text=number.toString()
+            Log.d("onClick","플러스 된 숫자는 $number")
+        }
+    }
+}
